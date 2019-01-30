1427d2332695a97d7789c83908aa6e29dc62481d
Gabriel Figueroa <gabriel.figueroa2020@gmail.com>
2019-01-30 16:53:10 -0400

    Project set up gradle

---
+++ hotel-reservation-angular-spring-boot/src/main/java/com/hotel/reservation/HotelReservationAngularSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.hotel.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationAngularSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ hotel-reservation-angular-spring-boot/src/test/java/com/hotel/reservation/HotelReservationAngularSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hotel.reservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelReservationAngularSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
